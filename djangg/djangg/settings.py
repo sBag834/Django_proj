e8b5094ffd11567361dc6a0f88ce70fb9d8244d3
sBag834 <snakseva71@gmail.com>
2024-11-16 16:03:03 +0400

    121123
    312123

--- djangg/djangg/settings.py
+++ djangg/djangg/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages'
 ]
 
 SITE_ID = 1
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
